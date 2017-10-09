476ca22754c93f7bfec5bec8e3b8b6824c6f6c37
Donegaan <donegaan@tcd.ie>
2017-10-09 22:05:24 +0100

    Initial Commit

---
+++ LCA-Sweng/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LCA-Sweng
+//
+//  Created by Andrew Donegan on 09/10/2017.
+//  Copyright © 2017 Andrew Donegan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

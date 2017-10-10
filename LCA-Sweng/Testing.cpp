7d808ae7de523bdbf49810d59e31b7ece59bdc09
Donegaan <donegaan@tcd.ie>
2017-10-10 20:36:14 +0100

    Added two more sections to the "Null" test case and commented out failed tests.

--- LCA-Sweng/Testing.cpp
+++ LCA-Sweng/Testing.cpp
@@ -15,7 +15,7 @@
 
 
 
-//Null test case
+//Test case for Null values
 
 TEST_CASE("Testing null values for all functions","null"){
     struct Node* testNode = (struct Node*) malloc(sizeof(struct Node));
@@ -23,8 +23,21 @@ TEST_CASE("Testing null values for all functions","null"){
     SECTION("Find path of Null"){
         std::vector<int> testPath;
         //To see if it will run with a null value passed as the int value.
-        REQUIRE((findPath(testNode, testPath, NULL))==false);
+        //
+        //REQUIRE((findPath(testNode, testPath, NULL))==false);
+        //
         // To see if it will run with a null value passed as the Node
         REQUIRE((findPath(NULL, testPath, 1))==false);
     }
+    
+    SECTION("Testing the LCA with a null value"){
+        // NULL node
+        REQUIRE((findLCA(NULL, 6, 2))==-1);
+    }
+    
+    SECTION("Testing to make sure you cannot access or create a null node"){
+        REQUIRE((testNode->key)==NULL);
+        //REQUIRE((newNode(NULL))==NULL);
+    }
 }
+
